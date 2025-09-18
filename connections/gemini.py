2602e3eb2262b28a5ea05b2da17135d8977c8bc9
senlyu <me@senlyu.com>
2025-09-17 21:44:59 -0700

    update prompt

--- connections/gemini.py
+++ connections/gemini.py
@@ -6,7 +6,7 @@ from typing import List
 from connections.model_api import ModelAPI
 
 class GeminiConnect(ModelAPI):
-    PROMOT = "用中文，第一部分尽可能简短的描述全球重大事件，美国重大事件，经济重大事件，短期股票市场，货币市场，重金属，大宗商品趋势，忽略重复的新闻。如果有政府，美联储官员等重要消息源头，再详述内容，确保明确保留其观点内容数据等详细信息，不可省略。第一部分时间包括昨日和今日。在第二部分，再用中文详细提供短期股票市场相关新闻，时间仅保留今日信息，保留详细数字和数据，重点保留美国数据，并且保留所有有关股票的数据和数字，股票代号用英文。使用<SP>作为分隔符,每大约1000字符的内容用<SP>来分开,每部分不可以超过1500字符"
+    PROMOT = "用中文，第一部分尽可能简短的描述全球重大事件，美国重大事件，经济重大事件，短期股票市场，货币市场，重金属，大宗商品趋势，忽略重复的新闻。如果有政府，美联储官员等重要消息源头，再详述内容，确保明确保留其观点内容数据等详细信息，不可省略。第一部分时间包括昨日和今日。在第二部分，再用中文详细提供短期股票市场相关新闻，时间仅保留今日信息，保留详细数字和数据，重点保留美国数据，并且保留所有有关股票的数据和数字，股票代号用英文。格式上全文使用<SP>作为分隔符,每大约1000字符的内容用<SP>来分开,分开的内容绝对不可以超过1500字符，不要因此省略输出的内容"
 
     @staticmethod
     def get_chunk_by_split(string: str, signal: str ="<SP>") -> List[str]:
@@ -30,8 +30,12 @@ class GeminiConnect(ModelAPI):
 
     def get_result_from_models(self, doc_paths):
         # try model "gemini-2.5-pro"
-        response = self.get_result_for_files(doc_paths, "gemini-2.5-pro")
-        msg = response.text
+        try:
+            response = self.get_result_for_files(doc_paths, "gemini-2.5-pro")
+            msg = response.text
+        except Exception as e:
+            Logging.log(e)
+            msg = None
 
         if (msg!="" and msg is not None):
             Logging.log("2.5 pro return results")
