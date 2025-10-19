703a0f53240f6d85f53bc584bcb6718e1f710082
senlyu <me@senlyu.com>
2025-10-19 10:10:37 -0700

    update main

--- src/main.py
+++ src/main.py
@@ -1,96 +1,40 @@
 import asyncio
-import pathlib
-from datetime import datetime, timedelta
-from telethon import TelegramClient
-from src.ai_utils.key_manager import KeyManager
-from .data_io.telegram import TelegramListener
-from .ai_utils.gemini import GeminiConnect, GeminiConnectKeyManager
-from .core.report_job import ReportJob
-from .data_io.discord import DiscordExporter
-from .data_io.hexo import HexoExporter
 from .util.logging_to_file import Logging
-from .util.sys_env import get_mode, get_is_dev_mode
+from .util.sys_env import get_mode
 from .util.config import Config
+from .util.components_factory import ComponentsFactory
 
-
-def init_telegram_listener_from_config(config):
-    (telegram_app_id, telegram_app_hash, telegram_client_name, telegram_channels) = config.get_telegram_config()
-    storage_path = config.get_storage_path_telegram()
-
-    pathlib.Path(storage_path).mkdir(parents=True, exist_ok=True) 
-    common_telegram_client = TelegramClient(telegram_client_name, telegram_app_id, telegram_app_hash)
-
-    return [TelegramListener(common_telegram_client, storage_path, channel, 5*60) for channel in telegram_channels]
-
-def init_discord_exporter_from_config(config):
-    discord_channel_url = config.get_discord_config()
-    return DiscordExporter(discord_channel_url)
-
-def init_hexo_exporter_from_config(config):
-    (path, post_path, url_domain, upload_command, command_path) = config.get_hexo_config()
-    return HexoExporter(path, post_path, url_domain, upload_command, command_path)
-
-def init_gemini_connect_from_config(config):
-    (gemini_api_key, gemini_history) = config.get_gemini_config()
-    return GeminiConnect(gemini_api_key, gemini_history)
-
-def init_gemini_connect_key_manager_from_config(config):
-    (_, gemini_history) = config.get_gemini_config()
-    return GeminiConnectKeyManager(init_gemini_key_manager_from_config(config), gemini_history)
-
-def init_gemini_key_manager_from_config(config):
-    gemini_keys = config.get_gemini_key_manager()
-    return KeyManager(gemini_keys, 1) # use one because token size limit
-
-def init_report_job_to_hexo(config):
-    storage_path = config.get_storage_path_telegram()
-    report_time = (datetime.now()+timedelta(seconds=10)).strftime("%H:%M:%S") if get_is_dev_mode() else "18:00:00"
-    return ReportJob(
-        "hexo report", 
-        report_time, 
-        init_hexo_exporter_from_config(config), 
-        init_gemini_connect_key_manager_from_config(config), 
-        storage_path, 
-        init_discord_exporter_from_config(config)
-    )
-
-async def main():
-    config = Config('config.json')
-    telegram_listeners = init_telegram_listener_from_config(config)
-    telegram_tasks = [
+async def gen_listeners(config):
+    factory = ComponentsFactory(config)
+    telegram_listeners = factory.init_telegram_listener_from_config()
+    return [
         await telegram_listener.start() for telegram_listener in telegram_listeners
     ]
 
+async def gen_reporters(config):
+    factory = ComponentsFactory(config)
     # upload task
-    # discord_job = init_report_job_to_discord(config)
-    hexo_job = init_report_job_to_hexo(config)
-
-    # all_tasks = telegram_tasks
-    all_tasks = telegram_tasks + [
-        # discord_job.start(), 
-        await hexo_job.start()
+    # discord_job = factory.init_report_job_to_discord()
+    hexo_job = factory.init_report_job_to_hexo()
+    return [
+        # await discord_job.start(), 
+        await hexo_job.start(),
     ]
 
+async def main():
+    config = Config('config.json')
+    all_tasks = []
+    all_tasks.extend(await gen_listeners(config))
+    all_tasks.extend(await gen_reporters(config))
     await asyncio.gather(*all_tasks)
 
 async def dev_on_listener():
     config = Config('config.json')
-    telegram_listeners = init_telegram_listener_from_config(config)
-    telegram_tasks = [
-        await telegram_listener.start() for telegram_listener in telegram_listeners
-    ]
-
-    all_tasks = telegram_tasks
-
-    await asyncio.gather(*all_tasks)
+    await asyncio.gather(*(await gen_listeners(config)))
 
 async def dev_on_reporter():
     config = Config('config.json')
-    hexo_job = init_report_job_to_hexo(config)
-
-    all_tasks = [await hexo_job.start()]
-
-    await asyncio.gather(*all_tasks)
+    await asyncio.gather(*(await gen_reporters(config)))
 
 if __name__ == "__main__":
     Logging.clean()
---
+++ src/util/components_factory.py
@@ -0,0 +1,64 @@
+
+
+import pathlib
+from datetime import datetime, timedelta
+from telethon import TelegramClient
+from src.ai_utils.key_manager import KeyManager
+from ..data_io.telegram import TelegramListener
+from ..ai_utils.gemini import GeminiConnect, GeminiConnectKeyManager
+from ..core.report_job import ReportJob
+from ..data_io.discord import DiscordExporter
+from ..data_io.hexo import HexoExporter
+from ..util.sys_env import get_is_dev_mode
+
+class ComponentsFactory:
+    def __init__(self, config):
+        self.config = config
+    
+    def init_telegram_listener_from_config(self):
+        config = self.config
+        (telegram_app_id, telegram_app_hash, telegram_client_name, telegram_channels) = config.get_telegram_config()
+        storage_path = config.get_storage_path_telegram()
+
+        pathlib.Path(storage_path).mkdir(parents=True, exist_ok=True) 
+        common_telegram_client = TelegramClient(telegram_client_name, telegram_app_id, telegram_app_hash)
+
+        return [TelegramListener(common_telegram_client, storage_path, channel, 5*60) for channel in telegram_channels]
+
+    def init_discord_exporter_from_config(self):
+        config = self.config
+        discord_channel_url = config.get_discord_config()
+        return DiscordExporter(discord_channel_url)
+
+    def init_hexo_exporter_from_config(self):
+        config = self.config
+        (path, post_path, url_domain, upload_command, command_path) = config.get_hexo_config()
+        return HexoExporter(path, post_path, url_domain, upload_command, command_path)
+
+    def init_gemini_connect_from_config(self):
+        config = self.config
+        (gemini_api_key, gemini_history) = config.get_gemini_config()
+        return GeminiConnect(gemini_api_key, gemini_history)
+
+    def init_gemini_connect_key_manager_from_config(self):
+        config = self.config
+        (_, gemini_history) = config.get_gemini_config()
+        return GeminiConnectKeyManager(self.init_gemini_key_manager_from_config(), gemini_history)
+
+    def init_gemini_key_manager_from_config(self):
+        config = self.config
+        gemini_keys = config.get_gemini_key_manager()
+        return KeyManager(gemini_keys, 1) # use one because token size limit
+
+    def init_report_job_to_hexo(self):
+        config = self.config
+        storage_path = config.get_storage_path_telegram()
+        report_time = (datetime.now()+timedelta(seconds=10)).strftime("%H:%M:%S") if get_is_dev_mode() else "18:00:00"
+        return ReportJob(
+            "hexo report", 
+            report_time, 
+            self.init_hexo_exporter_from_config(), 
+            self.init_gemini_connect_key_manager_from_config(), 
+            storage_path, 
+            self.init_discord_exporter_from_config()
+        )
