e3d654779602b15c66611b197ba8292e06ab1631
senlyu <me@senlyu.com>
2025-10-19 00:03:17 -0700

    setup env

--- src/util/sys_env.py
+++ src/util/sys_env.py
@@ -8,10 +8,10 @@ def get_is_dev_mode() -> bool:
     """
     return true if it is dev mode
     """
-    return (sys.argv[1] if len(sys.argv) > 1 else get_mode()) != "prod"
+    return get_mode() != "prod"
 
 def get_mode() -> str:
     """
     return mode, prod is default, default is empty
     """
-    return sys.argv[1] if len(sys.argv) > 1 else "prod"
+    return sys.argv[1] if len(sys.argv) > 1 and sys.argv[1]!="" else "prod"
