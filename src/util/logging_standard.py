f3308e4e398f15c0a99227567248a1321bdb0e73
senlyu <me@senlyu.com>
2025-11-15 13:28:33 -0800

    add logger clean

--- src/util/logging_standard.py
+++ src/util/logging_standard.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import os
 
 from src.util.session_context import SessionIDHelper
+from src.util.sys_env import get_is_dev_mode
 
 class Colors:
     """ANSI color codes for log formatting."""
@@ -66,17 +67,54 @@ class LoggingDirectoryHandle:
 class DefaultLogger:
     @staticmethod
     def getLogger(app_name="default", logging_level=logging.DEBUG, log_path="./log/standard"):
+        is_dev_mode = get_is_dev_mode()
+        if is_dev_mode:
+            log_path = "./"
+
         new_logger = logging.getLogger(app_name)
         new_logger.setLevel(logging_level)
 
         log_handler = LoggingDirectoryHandle(log_path) 
-        file_name = log_handler.get_today_file_name()
+        file_name = log_handler.get_today_file_name() if not is_dev_mode else os.path.join("./log.log")
         file_handler = logging.FileHandler(file_name)
         
         file_handler.setFormatter(ColoredFileFormatter())
         new_logger.addFilter(ContextSessionFilter())
         new_logger.addHandler(file_handler)
         return new_logger
+    
+    @staticmethod
+    def clean(log_path="./log/standard"):
+        is_dev_mode = get_is_dev_mode()
+        if is_dev_mode:
+            file_name = os.path.join("./log.log")
+            if os.path.isfile(file_name):
+                os.remove(file_name)
+            return
+
+        storage_path = os.path.join(log_path)
+        all_file_names = []
+        for root, _, files in os.walk(storage_path):
+            for file in files:
+                if file.lower().endswith('.log'):  # Case-insensitive check
+                    full_path = os.path.join(root, file)
+                    if os.path.isfile(full_path):
+                        all_file_names.append([file, full_path])
+        delete_targets = []
+        for item in all_file_names:
+            file_name = item[0]
+            date_in_file = file_name.split('.')[0]
+            try:
+                date_obj_file = datetime.strptime(date_in_file, '%Y-%m-%d')
+            except Exception as e:
+                print(e)
+                continue
+            
+            if date_obj_file + timedelta(days=30) < datetime.now():
+                delete_targets.append(item[1])
+        for full_path in delete_targets:
+            os.remove(full_path)
+            print(f"remove file: {full_path}")
     
 if __name__ == "__main__":
     logger = DefaultLogger.getLogger("test")
